14be30122f5e04073712792d41da4c948a3f66b8
Raj143 <rajshingade777@gmail.com>
2024-05-07 12:12:08 +0530

    [Raj] add : adding the alias of product category with as keyword

---
+++ SQLQuery11.sql
@@ -0,0 +1,14 @@
+SELECT
+    category_name 'Product Category'
+FROM
+    production.categories
+ORDER BY
+    category_name;  
+
+
+SELECT
+    category_name 'Product Category'
+FROM
+    production.categories
+ORDER BY
+    'Product Category';
